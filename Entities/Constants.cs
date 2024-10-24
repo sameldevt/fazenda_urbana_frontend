d2398d7800361650f463ea9cb31dde7d959e4c38
sameldevt <samuelsouza.dev@outlook.com>
2024-10-24 00:12:41 -0300

    feat: integração login e cadastro; tela de produtos

---
+++ Entities/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace fazenda_test.Entities
+{
+    public class Constants
+    {
+        public static readonly string BaseApiUrl = "http://18.188.227.244:8080/verdeviva";
+    }
+}
---
+++ Pages/Produtos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace fazenda_test.Pages
+{
+    public class ProdutosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
