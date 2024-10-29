9ac22ddc0a36e7a2dd4b68c182e5ce540c561ecb
sameldevt <samuelsouza.dev@outlook.com>
2024-10-29 17:35:12 -0300

    fix: card de produto ajustado

---
+++ Pages/DetalhesProduto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace fazenda_test.Pages
+{
+    public class DetalhesProdutoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
