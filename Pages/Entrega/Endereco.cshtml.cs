61d57251420e93c18397df87215e92a01f596567
sameldevt <samuelsouza.dev@outlook.com>
2024-11-11 19:12:09 -0300

    feat: fluxo de cadastro de endereço caso não exista nenhum

---
+++ Pages/Entrega/Endereco.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace fazenda_test.Pages.Entrega
+{
+    public class EnderecoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Entrega/Listar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace fazenda_test.Pages.Entrega
+{
+    public class ListarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
