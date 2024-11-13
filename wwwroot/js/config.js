1877a12732e3d02cd61a47058e6ee73409a9f89a
SSB-11 <shelles.borges@gmail.com>
2024-11-13 16:48:47 -0300

    feat: testes layout

--- wwwroot/js/config.js
+++ wwwroot/js/config.js
@@ -1,5 +1,5 @@
 // Modificar, caso o IP mude
-const API_URL = "http://34.204.80.6:8080";
+const API_URL = "http://3.219.74.46";
 //const API_URL = "http://localhost:5085";
 
 // Torna a variável acessível para outros arquivos .js
