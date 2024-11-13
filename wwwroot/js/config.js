03a95ff7bfb4f3659b7099bca338593c103ddf98
sameldevt <samuelsouza.dev@outlook.com>
2024-11-13 12:22:46 -0300

    fix: endereço da api alterado

--- wwwroot/js/config.js
+++ wwwroot/js/config.js
@@ -1,5 +1,5 @@
 // Modificar, caso o IP mude
-const API_URL = "http://52.54.99.249:8080";
+const API_URL = "http://34.204.80.6:8080";
 //const API_URL = "http://localhost:5085";
 
 // Torna a variável acessível para outros arquivos .js
